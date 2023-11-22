5148b60e9930b163c93e459a4eb634337ddd82f4
Elisson Vale <valeelisson@gmail.com>
2023-11-22 17:33:34 -0300

    Design parcialmente finalizado

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,9 +7,9 @@
         <meta name="description" content="" />
         <meta name="author" content="" />
         <title>Lastro BTC</title>
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <link rel="icon" type="image/x-icon" href="assets/image/favicon.png" />
         <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <script src="https://use.fontawesome.com/releases/v6.4.2/js/all.js" crossorigin="anonymous"></script>
         <!-- Google fonts-->
         <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
@@ -30,24 +30,27 @@
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#projects">Projects</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#signup">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#github">Github</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#signup">Contacts</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Documentation <i class="fas fa-file-code text-default"></i></a></li>
                     </ul>
                 </div>
             </div>
         </nav>
+
         <!-- Masthead-->
         <header class="masthead">
             <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
                 <div class="d-flex justify-content-center">
                     <div class="text-center">
                         <h1 class="mx-auto my-0 text-uppercase">Lastro BTC</h1>
-                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Free Bitcoin transactions, freedom and its sovereignty.</h2>
-                        <a class="btn btn-primary" href="#about">Download App</a>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Free Bitcoin transactions, to your freedom and sovereignty.</h2>
+                        <a class="btn btn-primary" href="#download">Download App</a>
                     </div>
                 </div>
             </div>
         </header>
+        
         <!-- About-->
         <section class="about-section text-center" id="about">
             <div class="container px-4 px-lg-5">
@@ -64,11 +67,12 @@
                 <img class="img-fluid" src="assets/image/ipad.png" alt="..." />
             </div>
         </section>
+
         <!-- Projects-->
-        <section class="projects-section bg-light" id="projects">
+        <section class="projects-section " id="github">
             <div class="container px-4 px-lg-5">
                 <!-- Featured Project Row-->
-                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                {{-- <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
                     <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/image/bg-masthead.jpg" alt="..." /></div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
@@ -76,7 +80,7 @@
                             <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
                         </div>
                     </div>
-                </div>
+                </div> --}}
                 <!-- Project One Row-->
                 <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
                     <div class="col-lg-6"><img class="img-fluid" src="assets/image/demo-image-01.jpg" alt="..." /></div>
@@ -107,6 +111,7 @@
                 </div>
             </div>
         </section>
+
         <!-- Signup-->
         <section class="signup-section" id="signup">
             <div class="container px-4 px-lg-5">
@@ -151,8 +156,9 @@
                 </div>
             </div>
         </section>
-        <!-- Contact-->
-        <section class="contact-section bg-black">
+
+         <!-- Contact -->
+         <section class="contact-section bg-black">
             <div class="container px-4 px-lg-5">
                 <div class="row gx-4 gx-lg-5">
                     <div class="col-md-4 mb-3 mb-md-0">
@@ -186,15 +192,50 @@
                         </div>
                     </div>
                 </div>
-                <div class="social d-flex justify-content-center">
+                {{-- <div class="social d-flex justify-content-center">
                     <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
                     <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
                     <a class="mx-2" href="#!"><i class="fab fa-github"></i></a>
+                </div> --}}
+            </div>
+        </section>
+
+        {{-- @yield('content') --}}
+        
+          <!-- Download -->
+        <section class="bg-black" id="download">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5" style="padding: 200px 0px;">
+                    <div class="col-md-6 mb-5 mb-md-0">
+                        <div class="card py-2 h-100">
+                            <div class="card-body text-center">
+                                <i class="fa-brands fa-android text-primary mb-2" style="font-size: 60px;"></i>
+                                <h4 class="text-uppercase m-0">Android</h4>
+                                <a href="#" class="btn btn-primary" style="margin: 20px;">Download</a>
+                                <hr class="my-2 mx-auto" />
+                                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 mb-5 mb-md-0">
+                        <div class="card py-2 h-100">
+                            <div class="card-body text-center">
+                                <i class="fa-brands fa-apple text-primary mb-2" style="font-size: 60px;"></i>
+                                <h4 class="text-uppercase m-0">Ios</h4>
+                                <a href="#" class="btn btn-primary" style="margin: 20px;">Download</a>
+                                <hr class="my-2 mx-auto" />
+                                <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
+
+       
          <!-- Footer-->
-        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Your Website 2023</div></footer>
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Lastro BTC 2023</div></footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
        
